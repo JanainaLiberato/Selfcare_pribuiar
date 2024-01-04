2257557832cce556499a7c23668920144d556209
Janaina Liberato <janaina_liberato@yahoo.com>
2024-01-04 12:21:28 -0300

    commit_sexto

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,11 +89,11 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
-    delay: 250,
+    delay: 200,
     // reset: true
 })
 
 sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
 sr.reveal(`.category__data, .trick__content, .footer__content`,{interval: 100})
 sr.reveal(`.about__data, .discount__img`,{origin: 'left'})
-sr.reveal(`.about__img, .discount__data`,{origin: 'right'})
+sr.reveal(`.discount__data`,{origin: 'right'})
